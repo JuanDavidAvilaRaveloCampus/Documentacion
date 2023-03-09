5c616170ac4a8070c044c7cb53dff5a636af3409
JuanAvila856 <juandavidavila856@gmail.com>
2023-03-08 23:12:55 -0500

    feat(Methods Change): :memo: Method .substring()
    add method .substring() whit it's description and it's two examples

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 let metodo = {
     "MÉTODO": "VALOR",
-    "slice": "Extrae una sección de una cadena y devuelve una nueva cadena."
+    "substring": "Devuelve el subconjunto especificado de la cadena, ya sea especificando los índices inicial y final o el índice inicial y una longitud."
 };
 console.table(metodo);
 
 // EJEMPLO 1
 //Devuelve la primera conincidencia de la palabra encontrada + su posición
 let ejemplo = "Hola soy un ejemplo";
-let otro = ejemplo.slice(2, -2);
+let otro = ejemplo.substring(0, 3);
 console.log(otro);
 
 // EJEMPLO 2
 let ejemplo2 = "Hola soy el siguiente ejemplo";
-let otro2 = ejemplo2.slice(0,8);
+let otro2 = ejemplo2.substring(15,1);
 console.log(otro2);
