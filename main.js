8a1145324f56399bfcb683143083c8a45767eb3d
JuanAvila856 <juandavidavila856@gmail.com>
2023-03-09 00:10:00 -0500

    feat(Methods Change): :memo: Method .replace()
    Add method .replace() with it's description and it's two examples

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 let metodo = {
     "MÉTODO": "VALOR",
-    ".repeat(n)": "Repite la cadena N veces"
+    ".replace(str|regex, newstr|func)": "Remplaza partes de un string por carácteres."
 };
 console.table(metodo);
 
 // EJEMPLO 1
-let ejemplo = "hola"
-let print = ejemplo.repeat(2)
+let ejemplo = "JavaScript"
+let print = ejemplo.replace("ava","-")
 console.log(print)
 
 // EJEMPLO_2 
-let ejemplo2 = "hola"
-let print2 = ejemplo2.repeat(7)
+let ejemplo2 = "Hello World"
+let print2 = ejemplo2.replace("ello", "-")
 console.log(print2)
