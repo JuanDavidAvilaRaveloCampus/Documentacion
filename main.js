82e84321c8e2e840d5e071aab1c14f41da0d4b10
JuanAvila856 <juandavidavila856@gmail.com>
2023-03-08 23:09:59 -0500

    feat(Methods Change): :memo: Method .slice()
    add method .slice() whit it's description and it's two examples

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 let metodo = {
     "MÉTODO": "VALOR",
-    "split": "Divide un objeto String en un arreglo de cadenas separando la cadena en subcadenas."
+    "slice": "Extrae una sección de una cadena y devuelve una nueva cadena."
 };
 console.table(metodo);
 
 // EJEMPLO 1
 //Devuelve la primera conincidencia de la palabra encontrada + su posición
 let ejemplo = "Hola soy un ejemplo";
-// let siguienteEjemplo = "soy Juan";
-let otro = ejemplo.split(" ");
+let otro = ejemplo.slice(2, -2);
 console.log(otro);
 
 // EJEMPLO 2
 let ejemplo2 = "Hola soy el siguiente ejemplo";
-// let siguienteEjemplo2 = "soy Pedro"
-let otro2 = ejemplo2.split(" ");
+let otro2 = ejemplo2.slice(0,8);
 console.log(otro2);
