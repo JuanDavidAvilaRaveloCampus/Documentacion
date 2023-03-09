441b78ec1daaff8ffab3efd185fbec8a5c806b5a
JuanAvila856 <juandavidavila856@gmail.com>
2023-03-09 00:32:03 -0500

    feat(Methods Change): :memo: Methods .trimEnd() and .trimStart()
    Adds the methods .trimEnd() and .trimStart() with their respectives descriptions in a table and their examples

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 let metodo = {
     "MÉTODO": "VALOR",
-    ".trim()": "Remueve los espacios en blanco de ambos extremos del string"
+    ".trimEnd()": "Remueve los espacios en blanco solo del final del string",
+    ".trimStart()": "Remueve los espacios en blanco solo del inicio del string"
 };
 console.table(metodo);
 
 // EJEMPLO 1
-let ejemplo = "          HI CAMPERS       "
-let print = ejemplo.trim()
-// Sin trin
+// Con trimEnd
+let ejemplo = "HI CAMPERS       "
+let print = ejemplo.trimEnd()
+console.log("\nCon .trinmEnd() no se logra apreciar el tanto el cambio, para verlo realmente\ntoca ver el código js :D\n ")
 console.log(ejemplo)
-// con trin
 console.log(print)
 
 // EJEMPLO_2 
+// Con trimStart
 let ejemplo2 = "           what's up nigga          "
-let print2 = ejemplo2.trim()
+let print2 = ejemplo2.trimStart()
 console.log(ejemplo2)
 console.log(print2)
