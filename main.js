e4e3897f189e3164561d5ad0e1b7ece71e1c1678
JuanAvila856 <juandavidavila856@gmail.com>
2023-03-08 23:33:24 -0500

    feat(Methods Change): :memo: Method .search()
    add method .search() with it's description and it's two examples

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 let metodo = {
     "MÉTODO": "VALOR",
-    "substring": "Devuelve el subconjunto especificado de la cadena, ya sea especificando los índices inicial y final o el índice inicial y una longitud."
+    ".search()": "Busca y retorna index"
 };
 console.table(metodo);
 
 // EJEMPLO 1
 //Devuelve la primera conincidencia de la palabra encontrada + su posición
 let ejemplo = "Hola soy un ejemplo";
-let otro = ejemplo.substring(0, 3);
+let otro = ejemplo.search("un");
 console.log(otro);
 
 // EJEMPLO 2
 let ejemplo2 = "Hola soy el siguiente ejemplo";
-let otro2 = ejemplo2.substring(15,1);
+let otro2 = ejemplo2.search("Hola");
 console.log(otro2);
