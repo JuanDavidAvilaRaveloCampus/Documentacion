de905cb8a092cf52da7c3f40bf8165cabf78c3f7
JuanAvila856 <juandavidavila856@gmail.com>
2023-03-08 23:47:57 -0500

    feat(Methods Change): :memo: Method .match()
    Add method .match() with it's description and it's two examples

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 let metodo = {
     "MÉTODO": "VALOR",
-    ".localeCompare(str, locale, options)": "Compara dos cadenas en la localización actual"
+    ".match(regex)": "Devuelve todas las ocurrencias de una expresión regular dentro de una cadena."
 };
 console.table(metodo);
 
 // EJEMPLO 1
 //Devuelve la primera conincidencia de la palabra encontrada + su posición
-let ejemplo = "Hola";
-let ejemplo_2 = "soy un ejemplo"
-let otro = ejemplo.localeCompare(ejemplo_2);
+let ejemplo = "Hola Soy Un Ejemplo";
+let ejemplo_2 = /[A-Z]/g;
+let otro = ejemplo.match(ejemplo_2);
 console.log(otro);
 
 // EJEMPLO 2
-let ejemplo2 = "Hola soy";
-let ejemplo2_2 = "el siguiente ejemplo"
-let otro2 = ejemplo2.localeCompare(ejemplo2_2);
+let ejemplo2 = "HoLa soy otro EJEMPLO";
+let ejemplo2_2 = /[A-Z]/g;
+let otro2 = ejemplo2.match(ejemplo2_2);
 console.log(otro2);
