6a18f2843a32f45e2a344d26b33efb04f4d8539d
JuanAvila856 <juandavidavila856@gmail.com>
2023-03-09 00:21:24 -0500

    feat(Methods Change): :art: Methods .toLoweCase() and .toUpperCase()
    Adds the methods .toLoweCase() and .toUpperCase() with their respectives descriptions in a table and their examples

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 let metodo = {
     "MÉTODO": "VALOR",
-    ".replace(str|regex, newstr|func)": "Remplaza partes de un string por carácteres."
+    ".toLowerCase()": "Devuelve el valor de la cadena convertida a minúsculas. No afecta el valor de la cadena misma.",
+    ".toUppercase()": "Devuelve el valor de la cadena convertida a mayúsculas. No afecta el valor de la cadena misma." 
 };
 console.table(metodo);
 
 // EJEMPLO 1
-let ejemplo = "JavaScript"
-let print = ejemplo.replace("ava","-")
+let ejemplo = "HI CAMPERS"
+let print = ejemplo.toLowerCase()
 console.log(print)
 
 // EJEMPLO_2 
-let ejemplo2 = "Hello World"
-let print2 = ejemplo2.replace("ello", "-")
+let ejemplo2 = "what's up nigga"
+let print2 = ejemplo2.toUpperCase()
 console.log(print2)
