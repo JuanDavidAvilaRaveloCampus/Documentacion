8a725edd5680e615c622fcede2421133542224eb
JuanAvila856 <juandavidavila856@gmail.com>
2023-03-08 23:06:47 -0500

    feat(Methods Change): :memo: Method .split()
    adds new method with it's description and it's two examples

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 let metodo = {
     "MÉTODO": "VALOR",
-    "concat": "Combina el texto de dos cadenas y devuelve una nueva cadena."
+    "split": "Divide un objeto String en un arreglo de cadenas separando la cadena en subcadenas."
 };
 console.table(metodo);
 
 // EJEMPLO 1
+//Devuelve la primera conincidencia de la palabra encontrada + su posición
 let ejemplo = "Hola soy un ejemplo";
-let siguienteEjemplo = "soy Juan";
-let otro = ejemplo.concat(' ',(siguienteEjemplo));
+// let siguienteEjemplo = "soy Juan";
+let otro = ejemplo.split(" ");
 console.log(otro);
 
 // EJEMPLO 2
 let ejemplo2 = "Hola soy el siguiente ejemplo";
-let siguienteEjemplo2 = "soy Pedro";
-let otro2 = ejemplo2.concat(" ",(siguienteEjemplo2));
+// let siguienteEjemplo2 = "soy Pedro"
+let otro2 = ejemplo2.split(" ");
 console.log(otro2);
