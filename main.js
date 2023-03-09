23095080a5ebfaf0b84f6182e64b9951126755aa
JuanAvila856 <juandavidavila856@gmail.com>
2023-03-08 22:44:36 -0500

    feat: :memo: Change string methods
    Metrods agree: .indexOf() and .lastIndexOf() with definitions and examples

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 let metodo = {
     "MÉTODO" : "VALOR",
-    "charAt, charCodeAt, codePointAt" : "Devuelve el caracter o código de caracter en la posición especificada en la cadena.",
+    "indexOf, lastIndexOf" : "Devuelve la posición de la subcadena especificada en la cadena o la última posición de la subcadena especificada, respectivamente."
 };
 console.table(metodo);
 
 // EJEMPLO 1
+//Devuelve la primera conincidencia de la palabra encontrada + su posición
 let ejemplo = "Hola soy un ejemplo";
-let otro = ejemplo.charAt(0)
-console.log(otro)
+let otro = ejemplo.indexOf("soy")
+console.log(otro) 
 
-// EJEMPLO 2
+// EJEMPLO 1
 let ejemplo2 = "Hola soy el siguiente ejemplo"
-let otro2 = ejemplo2.charCodeAt(3)
+let otro2 = ejemplo2.lastIndexOf("el")
 console.log(otro2)
