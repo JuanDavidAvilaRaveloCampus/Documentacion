946d443ef6ebafbbf78dee2e7c677264d6e4b05e
JuanAvila856 <juandavidavila856@gmail.com>
2023-03-08 22:59:13 -0500

    feat(Methods Change): :memo: New methods add
    Method add: .concat()

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 let metodo = {
     "MÉTODO": "VALOR",
-    "startsWith, endsWith, includes": "Devuelve si o no la cadena comienza, termina o contiene una subcadena especificada."
+    "concat": "Combina el texto de dos cadenas y devuelve una nueva cadena."
 };
 console.table(metodo);
 
 // EJEMPLO 1
-//Devuelve la primera conincidencia de la palabra encontrada + su posición
 let ejemplo = "Hola soy un ejemplo";
-let otro = ejemplo.startsWith("soy");
+let siguienteEjemplo = "soy Juan";
+let otro = ejemplo.concat(' ',(siguienteEjemplo));
 console.log(otro);
 
+// EJEMPLO 2
 let ejemplo2 = "Hola soy el siguiente ejemplo";
-let otro2 = ejemplo2.endsWith("ejemplo");
+let siguienteEjemplo2 = "soy Pedro";
+let otro2 = ejemplo2.concat(" ",(siguienteEjemplo2));
 console.log(otro2);
