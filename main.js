62fe6c8fa26fcd93e2872d8ae0ac1ee7c6d430c4
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-03-14 14:28:54 -0500

    feat(Function Change): :memo: Funciones flecha
    Breve descripción de las funciones flecha,  junto con un ejemplo

--- main.js
+++ main.js
@@ -1,25 +1,7 @@
 /**
- * *FUNCIONES ANÓNINAS o FUNCIONES LAMBDA
- *  Son un tipo de funcoines que se declaran sin nombre de función y se alojan en el interior
- *  de una variable y haciendo referencia a ella cada vez que queramos utilizarla:
+ * *FUNCIONES FLECHA:
+ *  Son la forma más reciente de escribir funciones en JavaScript. Se utilizan para crear 
+ *  funiones de una forma más conscisa.
  */
 
-//Funciòn anònima "saludo"
-const saludo = function(){
-    return "hola";
-};
-
-saludo; // f() { return 'Hola'; }
-saludo(); //'Hola'
-
-/**
- *  Observar que en la última ínea del ejemplo anterior, estamos ejecutando la variable, es decir
- *  ejecutando la función que contiene la variable. Sin embargo, el la línea anterior hacemos referencia 
- *  a la variable (sin ejecutarla, no hay paréntesis) y nos devuelve la función en sí.
- * 
- *  Este tipo de funciones sóolo están disponibles a partir de la inicialización de la variable.
- *  Si <<ejecutamos la variable>> antes de declararla, nos dará error.
- * 
- * 
- */
-
+let suma = (a, b) => a + b;
