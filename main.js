13b0b8e0f275959df24dac107f8f5b1aa616b8f8
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-03-14 15:00:38 -0500

    feat(Function Change): :memo: Funciones Autoejecutadas
    Descripción y ejemplos de las descripciones autoejecutadas

--- main.js
+++ main.js
@@ -1,78 +1,37 @@
 /**
- * *CALLBACKS(también llamadas funciones callback o retrollamadas);
- *  A grandes rasgos un callback(llamada hacia atrás) es pasar una 
- *  función B por parámetro a una función A, de modo que la función A 
- *  puede ejecutar esa función B de forrma genérica desde su código, y 
- *  nosotros podemos definirlas desde fuera de dicha función:
+ * *FUNCIONES AUTOEJECUTABLES
+ * 
+ *  Pueden eistir casos en los que necesites crear una función y ejecutarla sobre la marcha.
+ *  En JsvaScript es muy sencillo crear funciones autoejecutables. Básicamente, sólo tenemos que 
+ *  envolver entre paréntesis la función anónima en cuestión (no necesitamos que tenga nombre, 
+ *  puesto que no la vamos a guardar) y luego, ejecutarla:
  */
 
+// Función autoejecutable
+(function () {
+    console.log("Hola!!");
+  })();
+  
+  // Función autoejecutable con parámetros
+  (function (name) {
+    console.log(`¡Hola, ${name}!`);
+  })("Manz");
 
-// fB = Función B
-const fB = function() {
-    console.log("Funciòn B ejecutada.");
-};
-
-// fA = Función A
-const fA = function(callback){
-    callback();
-};
-
-fA(fB);
 
 /**
- *  Esto nos permitirá crear varias funciones para utilizar a modo de callback y reutilizarlas
- *  posteriormente con diferentes propósitos. De hecho, los callbacks muchas veces son la 
- *  primera estrategia que se suele utilizar en JavaScript para trabajar la asincronía, uno 
- *  de los temas que veremos más adelante:
+ *  Tambièn podemos utlizar parámetros en dichas funciones autoejecutables. Importante tener en cuenta
+ *  que los paràmetros se asignan al final de la función autoejecutable.
  */
 
-// fB = Función B (callback)
 /**
-    fB = Función B (callback)
-const fB = function () {
-    console.log("Función B ejecutada.");
-    };
-    
-// fError = Función Error (callback)
-    const fError = function () {
-    console.error("Error");
-    };
-    
-// fA = Función A
-    const fA = function (callback, callbackError) {
-    const n = ~~(Math.random() * 5);
-    if (n > 2) callback();
-    else callbackError();
-    };
-    
-    fA(fB, fError);   Si ejecutamos varias veces, algunas darán error y otras no
-*/
-
-/**
- * *Observando este ejemplo, podemos planear ejecutar la función fA() cambiandos los callbacks
- * *segùn nos interese, sin necesidad de crear funciones con el mismo código repetido una y otra vez.
- * *Además, en el caso de que las funciones callbacks sean muy cortas, muchas veces utilizamos 
- * *directamente la función anónima, sin necesidad de guardarla en una variable previamente:
+ * *Para tener en cuenta:
+ *  Si la funcion devuelve algún valor con return (a diferencia de los otros tipos de funciones); en este caso
+ *  lo que se almacena en la variable es el valor que develve la función autoejecutada:
  */
 
-// fA = Función A
-//const fA = function (callback, callbackError) {
-//    const n = ~~(Math.random() * 5);
-//    if (n > 2) callback();
-//    else callbackError();
-//  };
-//  
-//  fA(
-//    function () {
-//      console.log("Función B ejecutada.");
-//    },
-//    function () {
-//      console.error("Error");
-//    }
-//  );
-
-/**
- * *Aunque, como se pude ver, se suele evitar facilitar la legibilidad del código, y sólo se utiliza
- * *en casos muy específicos dodne estás seguro que no vas a reutilizar la función callback o no te 
- * *interesa guardarla en una variable.
- */
+const f = (function (name) {
+    return `¡Hola, ${name}!`;
+  })("Manz");
+  
+  f; // '¡Hola, Manz!`
+  typeof f; // 'string'
