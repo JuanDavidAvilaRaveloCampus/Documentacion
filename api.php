e52726ac5d454f697dac0e9bff09be7aadd4e866
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-05-17 13:56:10 -0500

    docs(PHP): :memo: Aricmetic Operator
    Se creó una pequeña "tabla" en la que se muestra por medio del documento las variables y sus resultados  utilizando los operadores aritméticos

---
+++ api.php
@@ -0,0 +1,41 @@
+<?php
+
+echo "<h1>ARICMETIC OPERATOR</h1>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$number1 = 10;
+$number2 = 20;
+$number3 = 30;
+$number4 = 40;
+
+echo "<h2>Variables</h2>";
+echo "number1 = 10 <br>";
+echo "number2 = 20 <br>";
+echo "number3 = 30 <br>";
+echo "number4 = 40 <br>";
+echo "<br>";
+echo "<br>";
+
+echo "<h2>Addition</h2>";
+$new_data = $number1 + $number2;
+echo "number1 + number2 = $new_data";
+
+echo "<h2>Subtraction</h2>";
+$new_data1 = $number1 - $number2;
+echo "number1 - number2 = $new_data1";
+
+echo "<h2>Multiplication</h2>";
+$new_data1 = $number1 * $number2;
+echo "number1 * number2 = $new_data1";
+
+echo "<h2>Divition</h2>";
+$new_data1 = $number3 / $number2;
+echo "number3 / number2 = $new_data1";
+
+
+
+
+
+?>
