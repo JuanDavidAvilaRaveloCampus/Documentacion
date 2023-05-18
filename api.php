84535d4bfb6180738e057d878e29b3f42f075c15
JuanDavidaAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-05-18 01:29:32 -0500

    feat(PHP): :memo: Scope Variables
    Definicion del alcance de las variables en PHP

--- api.php
+++ api.php
@@ -1,41 +1,17 @@
-<?php
+<?php 
 
-echo "<h1>ARICMETIC OPERATOR</h1>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
+// VARIABLES GLOBALES
+// El valor de estas variables no pueden ser obtenidas, ya que nu nivel de scope, aunque es
+// global, solo puede ser utilizado a cierto nivel, en este caso solo con el padre, ninguno por
+// debajo de el puede acceder al valor de esta variable, así como aplica con esta funcion, la 
+// cual retorna una variable undefined
+$valor = 34;
+function mostrar_data(){
+    echo "valor de variable %s".$valor;
+}
 
-$number1 = 10;
-$number2 = 20;
-$number3 = 30;
-$number4 = 40;
+echo "valor de variable %s".$valor;
 
-echo "<h2>Variables</h2>";
-echo "number1 = 10 <br>";
-echo "number2 = 20 <br>";
-echo "number3 = 30 <br>";
-echo "number4 = 40 <br>";
-echo "<br>";
-echo "<br>";
-
-echo "<h2>Addition</h2>";
-$new_data = $number1 + $number2;
-echo "number1 + number2 = $new_data";
-
-echo "<h2>Subtraction</h2>";
-$new_data1 = $number1 - $number2;
-echo "number1 - number2 = $new_data1";
-
-echo "<h2>Multiplication</h2>";
-$new_data1 = $number1 * $number2;
-echo "number1 * number2 = $new_data1";
-
-echo "<h2>Divition</h2>";
-$new_data1 = $number3 / $number2;
-echo "number3 / number2 = $new_data1";
-
-
-
-
+echo mostrar_data();
 
 ?>
