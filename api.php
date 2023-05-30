2f1af1f2842727d108d59e7bd98222a22b10084a
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-05-29 19:40:28 -0500

    docs(PHP): :memo: Algunos Metodos String
    Define y explica algunas de los principales métodos de string en php,  para mayor entendimiento cada método está comentado con su respectiva descripción y ejemplo debajo de el.

--- api.php
+++ api.php
@@ -1,10 +1,35 @@
-<?php 
+<?php
 
-$x = $_POST ["x"];
-$y = $_POST ["y"];
-// $z = $_POST ["z"];
-$total = null;
+    $nombreCliente = "Campers Campuslands";
 
-// $total = abs($x);
-$total = rand($x, $y);
-echo $total;
+    //Conocer el tamaño en carácteres de un String
+    echo strlen($nombreCliente)."<br>";
+    var_dump($nombreCliente)."<br>";
+
+    // Eliminar espacio en blanco
+    $texo = trim($nombreCliente)."<br>";
+    echo strlen($texo)."<br>";
+
+    //Convierte un String en Mayúsculas
+    echo strtoupper($nombreCliente)."<br>";
+
+    //Convierte un String en minúsculas
+    echo strtolower($nombreCliente)."<br>";
+
+    $mail_1 = "correo@correo.com";
+    $mail_2 = "Correo@correo.com";
+
+    var_dump(strtolower($mail_1) === strtolower($mail_2));
+    echo str_replace('Juan', 'J', $nombreCliente);
+
+    //Revisa si un string existe o no
+    echo strpos($nombreCliente, 'Pedro');
+
+    $tipoCliente = "Premium - Empresarial";
+
+    echo "<br>";
+    echo "<br>";
+
+    echo "El Cliente ".$nombreCliente." es ".$tipoCliente."<br>";
+    echo "El Cliente {$nombreCliente} es ${tipoCliente}";
+?>
